1bf7b4601fa7de901bbbcb7f5a12f1f778a4c03c
henry <hyi8656@gmail.com>
2026-01-09 17:27:37 +0900

    feat: 스토리북 제거

--- apps/docs/.storybook/manager.ts
+++
@@ -1,9 +0,0 @@
-import { addons } from 'storybook/manager-api';
-
-/**
- * @see {@link https://storybook.js.org/docs/configure/user-interface/theming#create-a-theme-quickstart}
- */
-addons.setConfig({
-  showPanel: true,
-  panelPosition: 'bottom',
-});
